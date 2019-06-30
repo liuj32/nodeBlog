46855ef8c860b2e893ad21f52da7630b4d43dc3b
liujie <593177117@qq.com>
2019-06-30 10:59:57 +0800

    1

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const flash = require('connect-flash')
 const config = require('config-lite')(__dirname)
 const routes = require('./routes')
 const pkg = require('./package')
+require('objectid-to-timestamp')
 
 const app = express()
 
@@ -32,6 +33,12 @@ app.use(session({
 
 //flash中间件，用来显示通知
 app.use(flash())
+
+// 处理表单及文件上传的中间件
+app.use(require('express-formidable')({
+  uploadDir: path.join(__dirname, 'public/img'), // 上传文件目录
+  keepExtensions: true// 保留后缀
+}))
 
 // 设置模板全局常量
 app.locals.blog = {
